5f9c9edaa731934725b82c0aeb5aaf538c8f97b3
Gurugk20 <gurukiranm2010@gmail.com>
2025-08-12 11:31:18 +0530

    change.py changed

--- change.py
+++ change.py
@@ -1,9 +1,16 @@
-from login_handler import setup_session, handle_auth_flow, render_login_page
+from login_handler import setup_session, handle_auth_flow, ensure_not_expired, render_login_page, render_request_access_page
 import streamlit as st
 
 setup_session()
 handle_auth_flow()
+ensure_not_expired()
 
 if not st.session_state.logged_in:
-    render_login_page()
-    st.stop()
+    if st.session_state.get("access_denied_email"):
+        # User authenticated but not allowlisted → show Request Access flow
+        render_request_access_page()
+        st.stop()
+    else:
+        # Normal login screen
+        render_login_page()
+        st.stop()
